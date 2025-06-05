7033af4921cbb1d363fa328a765ce110b0c5d69b
Avinash Gupta <avinash12231@gmail.com>
2025-06-05 16:46:15 +0530

    Third

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.post("/upload",upload.single("file"),(req,res)=>{
     res.end("File uploaded!");
 });
 
-app.listen(3000,()=>{
+app.listen(process.env.PORT,()=>{
     console.log("Server is running on port 3000");
 })
 
