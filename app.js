6e93167a412af441b9f163c90320afb7bd6433f6
Avinash Gupta <avinash12231@gmail.com>
2025-06-05 16:29:56 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const multer = require("multer");
+const bodyParser = require('body-parser');
+const crypto = require('crypto');
+const storage = require("./multer.middleware.js");
+const path = require("node:path");
+
+const app = express();
+const upload = multer({storage : storage});
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/",(req,res)=>{
+    res.render("index");
+});
+
+app.post("/upload",upload.single("file"),(req,res)=>{
+    console.log(req.file);
+    res.end("File uploaded!");
+});
+
+app.listen(3000,()=>{
+    console.log("Server is running on port 3000");
+})
+
+
---
+++ multer.middleware.js
@@ -0,0 +1,17 @@
+const multer = require("multer");
+const path = require("path");
+const crypto = require("crypto");
+
+const storage = multer.diskStorage({
+    destination : function(req, file, cb){
+        cb(null, "./public/uploads");
+    },
+    filename : function(req,file,cb){
+        crypto.randomBytes(12,function(err,bytes){
+            if (err) return null;
+            cb(null, bytes.toString("hex") + path.extname(file.originalname));
+        });
+    }
+});
+
+module.exports = storage;
