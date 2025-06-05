50e92c128cc4bb1e3b536942d8b832c96e1dd266
Avinash Gupta <avinash12231@gmail.com>
2025-06-05 16:45:23 +0530

    Second

--- multer.middleware.js
+++ multer.middleware.js
@@ -4,7 +4,7 @@ const crypto = require("crypto");
 
 const storage = multer.diskStorage({
     destination : function(req, file, cb){
-        cb(null, "./public/uploads");
+        cb(null, path.join(__dirname,"public/uploads"));
     },
     filename : function(req,file,cb){
         crypto.randomBytes(12,function(err,bytes){
